4024face52f44ff3a62c84e77bb4116f3602b201
montoyamoraga <montoyamoraga@gmail.com>
2020-09-27 22:08:38 -0400

    add more arduino code for wrap around of millis

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -7,26 +7,37 @@ unsigned long previousMillis = 0;
 unsigned long currentMillis = 0;
 
 // 1 minute
-const long interval = 1000 * 60;
+// const long interval = 1000 * 60;
 
 // 1 hour
-// const long interval = 1000 * 60 * 60;
+const long interval = 1000 * 60 * 60;
 
 // 1 day
 // const long interval = 1000 * 60 * 60 * 24;
 
 void setup() {
   Serial.begin(9600);
-
 }
 
 void loop() {
 
+  // update currentMillis to the current time
   currentMillis = millis();
 
+  // check if time passed since
   if (currentMillis - previousMillis >= interval) {
+
+    // update previousMillis to the current time
     previousMillis = currentMillis;
+
+
     Serial.println("hello world");
+  }
+  // check if millis() did a a wrap around
+  else if (currentMillis - previousMillis < 0) {
+
+    // // update previousMillis to the current time
+    previousMillis = currentMillis;
   }
 
 }
