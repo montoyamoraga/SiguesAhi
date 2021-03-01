310b006688e2f5344a1866caf76b5a783984a090
montoyamoraga <montoyamoraga@gmail.com>
2021-02-28 20:04:51 -0500

    add conditional compilation

--- src/sigues_ahi.h
+++ src/sigues_ahi.h
@@ -7,7 +7,14 @@
 // alpha state
 // it is working but it is messy
 
+// conditional compilation
+#ifndef SIGUES_AHI_H
+#define SIGUES_AHI_H
+
 // include libraries
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <ArduinoJson.h>
+
+// conditional compilation
+#endif
