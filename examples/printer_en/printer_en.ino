06ab7e5ea21ac567f98b078b15094dc4f3003c4a
montoyamoraga <montoyamoraga@gmail.com>
2021-05-23 21:56:03 -0400

    create placeholders for screen and printer, in english and spanish

---
+++ examples/printer_en/printer_en.ino
@@ -0,0 +1,23 @@
+// include library
+#include <SiguesAhi.h>
+
+// include file for internet login
+#include "secret.h"
+
+// instance of class
+SiguesAhi sigues;
+
+// variables to be set by user
+String myWikiPageTitle = "National_Rifle_Association";
+// 70101: NRA
+int myWikiPageID = 70101;
+// true for printing in console, false for
+bool myDebugging = true;
+
+void setup() {
+
+}
+
+void loop() {
+
+}
