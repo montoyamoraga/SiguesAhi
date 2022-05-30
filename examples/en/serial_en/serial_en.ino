c998c311d493fca41f3e666ff948433ad66c9513
aarón montoya-moraga <montoyamoraga@gmail.com>
2022-05-30 17:38:16 -0400

    delete my from variables

--- examples/en/serial_en/serial_en.ino
+++ examples/en/serial_en/serial_en.ino
@@ -2,45 +2,39 @@
 #include <SiguesAhi.h>
 
 // include file for internet login
-// #include "secret.h"
+#include "secret.h"
 
 // or put your info here
 #ifndef NETWORK_NAME
-#define NETWORK_NAME "replaceHere"
+#define NETWORK_NAME "replace"
 #endif
 
 #ifndef NETWORK_PASS
-#define NETWORK_PASS "replaceHere"
+#define NETWORK_PASS "replace"
 #endif
 
 // instance of class
 SiguesAhi sigues(OUTPUT_SERIAL);
 
 // variables to be set by user
-String myWikiPageTitle = "National_Rifle_Association";
+String wikiPageTitle = "National_Rifle_Association";
 // 70101: NRA
-int myWikiPageID = 70101;
+int wikiPageID = 70101;
 // true for printing in console, false for
-bool myDebugging = true;
+bool debugging = true;
 
 void setup() {
 
-  sigues.setDebugging(myDebugging);
+  sigues.setDebugging(debugging);
 
   // initialize serial and wait for port to open
   // argument are NETWORK_NAME and NETWORK_PASS, from secret.h
   sigues.setNetwork(NETWORK_NAME, NETWORK_PASS);
 
-  sigues.setWiki(myWikiPageTitle, myWikiPageID);
+  // set wikipedia
+  sigues.setWiki(wikiPageTitle, wikiPageID);
 
-  // TODO: document
-  sigues.connectSSL();
-
-  sigues.checkStatus();
-
-  sigues.skipHeaders();
-
-  sigues.parseJSON();
+  sigues.checkExistence();
 
   sigues.printExistence();
 }
@@ -49,8 +43,6 @@ void loop() {
 
   // if there are incoming bytes available
   // from the server, read them and print them:
-
   sigues.isClientAvailable();
-
   sigues.isClientConnected();
 }
